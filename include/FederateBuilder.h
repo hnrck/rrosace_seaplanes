7a7a88bf161d698ba7c49bef4542b55cedc48e73
Henrick Deschamps <henrick.deschamps@gmail.com>
2019-12-29 21:46:38 +0900

    Creating a builder for federate

--- include/FederateBuilder.h
+++ include/FederateBuilder.h
@@ -7,6 +7,37 @@
 #ifndef RROSACE_SEAPLANES_FEDERATEBUILDER_H
 #define RROSACE_SEAPLANES_FEDERATEBUILDER_H
 
-class FederateBuilder final {};
+#include <Common.h>
+#include <Configuration.h>
+#include <Federate.h>
+
+class FederateBuilder final {
+private:
+  Name __federation_{};
+  Name __federate_{};
+  Name __fom_{};
+  Seaplanes::SeaplanesTime __end_time_{0.};
+
+  VecUpModels __models_{};
+
+  // TODO values to print
+public:
+  FederateBuilder();
+  virtual ~FederateBuilder();
+
+  FederateBuilder(const FederateBuilder &) = delete;
+  FederateBuilder &operator=(const FederateBuilder &) = delete;
+
+  FederateBuilder(FederateBuilder &&) = default;
+  FederateBuilder &operator=(FederateBuilder &&) = default;
+
+  FederateBuilder &setFederation(const Name &federation);
+  FederateBuilder &setFederate(const Name &federate);
+  FederateBuilder &setFom(const Name &fom);
+  FederateBuilder &setEndTime(const Seaplanes::SeaplanesTime &endTime);
+  FederateBuilder &addModel(UpModel &&up_model);
+
+  UpFederate build();
+};
 
 #endif // RROSACE_SEAPLANES_FEDERATEBUILDER_H
--- src/FederateBuilder.cpp
+++ src/FederateBuilder.cpp
@@ -5,3 +5,48 @@
 //! \brief Federate builder body
 
 #include <FederateBuilder.h>
+#include <ModelFactory.h>
+
+FederateBuilder::FederateBuilder() = default;
+FederateBuilder::~FederateBuilder() = default;
+
+FederateBuilder &FederateBuilder::setFederation(const Name &federation) {
+  __federation_ = federation;
+  return *this;
+}
+
+FederateBuilder &FederateBuilder::setFederate(const Name &federate) {
+  __federate_ = federate;
+  return *this;
+}
+
+FederateBuilder &FederateBuilder::setFom(const Name &fom) {
+  __fom_ = fom;
+  return *this;
+}
+
+FederateBuilder &
+FederateBuilder::setEndTime(const Seaplanes::SeaplanesTime &endTime) {
+  __end_time_ = endTime;
+  return *this;
+}
+
+FederateBuilder &FederateBuilder::addModel(UpModel &&up_model) {
+  __models_.push_back(std::move(up_model));
+  return *this;
+}
+
+UpFederate FederateBuilder::build() {
+  // TODO implement
+  auto dt = 0.;
+
+  for (auto &model : __models_) {
+    if (dt > model->get_dt()) {
+      dt = model->get_dt();
+    }
+  }
+
+  return std::make_unique<Federate>(std::move(__federation_),
+                                    std::move(__federate_), std::move(__fom_),
+                                    std::move(__models_), __end_time_, dt);
+}
