3833e79f3be3346b52b8794fdc55b95ca3094a98
Henrick Deschamps <henrick.deschamps@gmail.com>
2019-12-29 21:47:05 +0900

    Adding the base federate

--- include/Federate.h
+++ include/Federate.h
@@ -7,10 +7,18 @@
 #ifndef RROSACE_SEAPLANES_FEDERATE_H
 #define RROSACE_SEAPLANES_FEDERATE_H
 
+#include <Common.h>
+#include <FederateBuilder.h>
 #include <ProtoLogicalProcessor.h>
 
 class Federate final : Seaplanes::ProtoLogicalProcessor {
+private:
+  VecUpModels __up_models_;
+
 public:
+  Federate(Name federation, Name federate, Name fom, VecUpModels up_models,
+           Seaplanes::SeaplanesTime end_time, double timestep);
+
   void localsCalculation() final;
   void initDumpFiles() final;
   void dumpValuesInFiles() final;
--- src/Federate.cpp
+++ src/Federate.cpp
@@ -4,4 +4,27 @@
 //! \date December 2019
 //! \brief Federate body
 
+#include <Configuration.h>
 #include <Federate.h>
+#include <FederateBuilder.h>
+#include <ModelFactory.h>
+
+#include <rrosace.h>
+
+Federate::Federate(Name federation, Name federate, Name fom,
+                   VecUpModels up_models, Seaplanes::SeaplanesTime end_time,
+                   double timestep)
+    : Seaplanes::ProtoLogicalProcessor(
+          std::move(federation), std::move(federate), std::move(fom),
+          end_time.get_s(), timestep, timestep, Name()),
+      __up_models_{std::move(up_models)} {}
+
+void Federate::localsCalculation() {
+  // TODO check scheduling
+  for (auto &up_model : __up_models_) {
+    up_model->step();
+  }
+}
+
+void Federate::initDumpFiles() {}
+void Federate::dumpValuesInFiles() {}
