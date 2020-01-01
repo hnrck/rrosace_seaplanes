769fc75b80c23a9fd7d9fb85aa8ec379fd4422e5
Henrick Deschamps <henrick.deschamps@gmail.com>
2020-01-02 08:28:49 +0900

    Populating seaplanes object classes and instances

--- src/Federate.cpp
+++ src/Federate.cpp
@@ -16,20 +16,34 @@ Federate::Federate(Name federation, Name federate, Name fom,
           std::move(federation), std::move(federate), std::move(fom),
           end_time.get_s(), time_step, time_step, Name()),
       __up_models_{std::move(up_models)} {
+
   for (auto &status_creators_tuple :
        Values::get_instance().status_creators_tuples_array) {
+
     if (std::get<0>(status_creators_tuple).state ==
         Values::Status::State::PUBLISH) {
       __updaters_.push_back(std::get<1>(status_creators_tuple)(
           __sp_objects_map_, __up_instances_published_map_));
     }
+
     if (std::get<0>(status_creators_tuple).state ==
         Values::Status::State::SUBSCRIBE) {
       __retrievers_.push_back(std::get<2>(status_creators_tuple)(
           __sp_objects_map_, __up_instances_subscribed_map_));
     }
   }
-  // TODO move objects
+
+  for (auto &sp_object : __sp_objects_map_) {
+    addObjectClass(std::move(sp_object.second));
+  }
+
+  for (auto &up_instances_published : __up_instances_published_map_) {
+    addPublishedObject(std::move(up_instances_published.second));
+  }
+
+  for (auto &up_instances_subscribed : __up_instances_subscribed_map_) {
+    addSubscribedObject(std::move(up_instances_subscribed.second));
+  }
 }
 
 void Federate::localsCalculation() {
